b999eb9cc143dab59e6416a817c192d55397ae0c
Meg <meggoodwin57@gmail.com>
2021-03-21 01:24:36 -0500

    fixed search results display and DRY-ed out the job listings by turning it into a fragment (completed TODOs #3 & #4)

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,18 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
